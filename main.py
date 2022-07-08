037da11825d604868ba07d085ef0422bdd8aa6a6
Fletcher <me@fboyd.me>
2022-07-09 00:32:02 +0800

    Add notification types

--- main.py
+++ main.py
@@ -2,6 +2,40 @@
 import requests
 from bs4 import BeautifulSoup
 
+def alert(trigger="always", threshold=3,pos=False):
+    """triggers
+    not_on_front - Send a notification if we're not on the first page
+    threshold - Send a notification if we're ranked higher than X
+    always - Send a notification no matter what
+    """
+    if trigger == "not_on_front":
+        if pos == False:
+            notify("We're not on the front page anymore!")
+
+    if trigger == "threshold":
+        if pos == False:
+            notify("We're not on the front page anymore!")
+        elif pos > threshold:
+            notify("We're no longer in the top {}!".format(threshold)) 
+
+    if trigger == "always":
+        if pos == False:
+            notify("We're not on the front page anymore!")
+        elif pos != 1:
+            print("We're currently {} on the recruitment forum. Time for a bump?".format(pos+1))
+        else:
+            print("We're #1, we're #1!")
+
+
+def notify(s):
+    print(s)
+
+def rank(clans):
+    p = 1
+    for clan in clans:
+        print("#{}: {}".format(p,clan))
+        p += 1
+
 # thread to check
 url = "https://secure.runescape.com/m=forum/forums?290,291,thd,908,66254470"
 
@@ -20,18 +54,11 @@ for thread in thread_titles:
     clans.append(thread.get_text())
 
 # check if the clan is on the list
-try:
-    pos = clans.index(clan_str)
-    if pos != 0:
-        print("We're currently {} on the recruitment forum. Time for a bump?".format(pos+1))
-    else:
-        print("We're #1, we're #1!")
-except:
-    print("We're not on the front page, definitely time for a bump.")
 
-# print a ranking
-print("-----")
-p = 1
-for clan in clans:
-    print("#{}: {}".format(p,clan))
-    p += 1
+pos = False
+try:
+    pos = clans.index(clan_str) + 1
+except:
+    pass
+
+alert(trigger="threshold", threshold=3,pos=pos)
